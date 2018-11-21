785194dfda1c7812d0fecb7a3d90eca427bb1397
zero <condorheroes1995@163.com>
2018-11-21 11:49:34 +0800

    [add] add sparksession

--- src/main/scala/APP.scala
+++ src/main/scala/APP.scala
@@ -1,3 +1,6 @@
 object APP {
 
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSessionHolder.sparkSession;
+  }
 }
---
+++ src/main/scala/SparkSessionHolder.scala
@@ -0,0 +1,19 @@
+import org.apache.spark.sql.SparkSession
+
+object SparkSessionHolder {
+
+  private var _sparkSession: SparkSession = null
+
+  def build(): SparkSession = {
+    _sparkSession = SparkSession.builder.master("local[*]").appName("ETLApp").getOrCreate()
+    _sparkSession
+  }
+
+  def sparkSession: SparkSession = {
+    if (_sparkSession == null) {
+      build()
+    } else _sparkSession
+  }
+
+
+}
